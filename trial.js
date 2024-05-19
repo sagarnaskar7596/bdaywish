f98136730c4723c241209c754e13021dc7ab145b
sagarnaskar7596 <sagar.naskar7596872078@gmail.com>
2024-05-19 12:47:24 +0530

    change

---
+++ trial.js
@@ -0,0 +1,27 @@
+// document.getElementById('showAnimation').addEventListener('click', function() {
+//     const giftBox = document.getElementById('giftBox');
+//     const cake = document.getElementById('cake');
+
+//     // Reset styles to initial state
+//     giftBox.style.display = 'none';
+//     cake.style.display = 'none';
+
+//     // Start animation
+//     giftBox.style.display = 'block';
+//     setTimeout(() => {
+//         giftBox.style.display = 'none';
+//         cake.style.display = 'block';
+//     }, 2000); // Adjust time as needed
+// });
+
+    function checkScreenWidth() {
+        if (window.innerWidth < 1120) { // Adjust the pixel value as needed
+            window.location.href = 'trial.html';
+        }
+    }
+    
+    // Check screen width on window resize
+    window.addEventListener('resize', checkScreenWidth);
+    
+    // Initial check on page load
+    document.addEventListener('DOMContentLoaded', checkScreenWidth);
